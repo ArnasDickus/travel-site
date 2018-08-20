df68023d48fe23f153e70a18ef76d7fee8d658c8
Arnas <1arnasdickus1@gmail.com>
2018-08-20 19:18:15 +0300

    Searching for bug

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,14 +2,14 @@ var gulp = require('gulp'),
 watch = require('gulp-watch'),
 browserSync = require('browser-sync').create();
 
-
 gulp.task('watch', function() {
+
   browserSync.init({
     notify: false,
-    server:{
+    server: {
       baseDir: "app"
     }
-  })
+  });
 
   watch('./app/index.html', function() {
     browserSync.reload();
@@ -21,7 +21,7 @@ gulp.task('watch', function() {
 
 });
 
-gulp.task('cssInject', ['styles'], function(){
+gulp.task('cssInject', ['styles'], function() {
   return gulp.src('./app/temp/styles/styles.css')
-  .pipe(browserSync.stream());
+    .pipe(browserSync.stream());
 });
